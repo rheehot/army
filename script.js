ad53f394c241b722d4c32195fca10ec5eb40a5f9
Mingeun Kim <mingeun.k.k@gmail.com>
2020-07-23 13:39:06 +0900

    refactor function naming and split function role

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-const go = 1598850000000
-const out = 1651294800000
+const ENLISTMENT_AT = 1598850000000
+const DISCHARGED_AT = 1651294800000
 
-
-function fromNow(){
+function convertDayFromEnlistmentAndDischargeDate() {
     const now = Math.round(new Date().getTime())
-    let time
-    if(go >= now) time = go
-    else time = out
-    time = (time - now)
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((time % (1000 * 60)) / 1000);
 
-    return { time: `${days}일 ${hours}시간 ${minutes}분 ${seconds}초`, army: go < now }
+    return ENLISTMENT_AT < now;
 }
 
+function buildDateCount() {
+    const now = Math.round(new Date().getTime())
+
+    let time
+    if (ENLISTMENT_AT >= now) time = ENLISTMENT_AT
+    else time = DISCHARGED_AT
+    time -= now
+
+    const DAYS = Math.floor(time / (1000 * 60 * 60 * 24));
+    const HOURS = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const MINUTES = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const SECONDS = Math.floor((time % (1000 * 60)) / 1000);
+
+    return `${DAYS}일 ${HOURS}시간 ${MINUTES}분 ${SECONDS}초`;
+}
 
 window.addEventListener("load", () => {
     setInterval(()=> {
-        data = fromNow()
-        document.querySelector('h1').innerText = data.time
-        document.querySelector('h2').innerText = '케빈님의 ' + (data.army ? '전역까지' : '입대까지')
+        let army = convertDayFromEnlistmentAndDischargeDate()
+        document.querySelector('h1').innerText = buildDateCount()
+        document.querySelector('h2').innerText = (army ? '전역까지' : '입대까지')
     }, 1000)
-    
-  });
+  }
+);
